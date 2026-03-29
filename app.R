34856914e9a94278ed39ac011a0605924b6cc1c5
samfeinberg <samgfeinberg@gmail.com>
2026-03-28 21:44:55 -0400

    Added/tested demo code from Shiny

--- app.R
+++ app.R
@@ -2,3 +2,18 @@
 
 # Sam Feinberg
 
+
+n <- 200
+
+ui <- bootstrapPage(
+  numericInput('n', 'Number of obs', n),
+  plotOutput('plot')
+)
+
+server <- function(input, output) {
+  output$plot <- renderPlot({
+    hist(runif(input$n))
+  })
+}
+
+shinyApp(ui = ui, server = server)
