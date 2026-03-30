486ab6d12742d5c9bd0bc52d511daf3edd0f6cf3
samfeinberg <samgfeinberg@gmail.com>
2026-03-29 21:49:58 -0400

    Updated code to comply with directions on Canvas

--- app.R
+++ app.R
@@ -2,25 +2,49 @@
 
 # Sam Feinberg
 
+library(shiny)
 
 n <- 200
 
 ui <- bootstrapPage(
   h2('Sam Feinberg'),
   numericInput('n', 'Number of obs', n),
+  selectInput(
+    "type",
+    "Choose plot type",
+    choices = c("Histogram", "Boxplot")
+  ),
+  selectInput(
+    "col",
+    "Choose color",
+    choices = c("green", "red", "blue", "gray", "orange")
+  ),
   plotOutput(outputId = 'boxplot')
 )
 
 server <- function(input, output) {
   output$boxplot <- renderPlot({
-    boxplot(runif(input$n),
-            col = 'red')
-  })
-}
+    data <- runif(input$n)
+    if (input$type == "Histogram") {
+      hist(
+        data,
+        col = input$col,
+        main = "Histogram"
+        )
+    } 
+    else {
+      boxplot(
+        data,
+        main = "Boxplot",
+        col = input$col
+            )
+    }}
+  )
+  }
+
 
 shinyApp(ui = ui, server = server)
 
-# To change the plot from histogram to boxplot, I changed the plotOutud outputId to
-# be 'boxplot'. Then I used that outputId with renderPlot to render the boxplot. I added
-# an argument 'col' inside the boxplot to make it red. I used h2 in the UI to put my name
-# at the top in header 2 size.
+# To make the type of plot changeable, I used selectInput with 2 choices, histogram and
+# boxplot. Then, I used selectInput for the color as well, so it can be picked from a list.
+# I used h2 in the UI to put my name at the top in header 2 size.
